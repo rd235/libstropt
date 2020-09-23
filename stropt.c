e42ddf27d4b202afc9a3d2dad3f205da6d7f94a3
Renzo Davoli <renzo@cs.unibo.it>
2020-09-23 08:20:19 +0200

    License header mismatch

--- stropt.c
+++ stropt.c
@@ -1,20 +1,15 @@
 /*
  *   stropt: Parse options from a string (supports quotation, option arguments, no malloc needed for parsing)
  *
- *   Copyright (C) 2018  Renzo Davoli <renzo@cs.unibo.it> VirtualSquare team.
+ *   Copyright (C) 2018/2020  Renzo Davoli <renzo@cs.unibo.it> VirtualSquare team.
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 2 of the License, or
- *   (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation; either version 2.1 of the License, or (at
+ *   your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, see <http://www.gnu.org/licenses/>.
  *
  */
 
